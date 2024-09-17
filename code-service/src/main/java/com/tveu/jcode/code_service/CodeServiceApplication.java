8982b6db0f9ef2c6dcb0ec7c3b68ad34acaf0eff
St3plox <st3pegor@gmail.com>
2024-09-17 19:48:28 +0700

    code service template

---
+++ code-service/src/main/java/com/tveu/jcode/code_service/CodeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tveu.jcode.code_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeServiceApplication.class, args);
+	}
+
+}
---
+++ code-service/src/test/java/com/tveu/jcode/code_service/CodeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tveu.jcode.code_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
