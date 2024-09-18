1aab208025550ef68eef1b62c0f3ac2775fedf26
St3plox <st3pegor@gmail.com>
2024-09-18 10:09:52 +0700

    working eureka and gateway

--- api-gateway/src/main/java/com/tveu/jcode/gateway/GatewayApplication.java
+++ api-gateway/src/main/java/com/tveu/jcode/gateway/GatewayApplication.java
@@ -2,10 +2,10 @@ package com.tveu.jcode.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
 public class GatewayApplication {
 
     public static void main(String[] args) {
